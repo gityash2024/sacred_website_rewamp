d450e5722a99603c3c21c062dbafd357bc37c874
akansha bhardwaj <akansha@ith.tech>
2025-11-19 20:10:50 +0530

    add image bussion & pepole page

--- src/pages/BusinessForPlanet/BusinessForPlanet.tsx
+++ src/pages/BusinessForPlanet/BusinessForPlanet.tsx
@@ -3,6 +3,49 @@
  */
 import { SEO } from '@/components/common/SEO'
 import styles from './BusinessForPlanet.module.css'
+import Whatisgoodfornaturecanbegoodforyourbusiess from '@/assets/Whatisgoodfornaturecanbegoodforyourbusiness.png' 
+import Ringboxcopy from '@/assets/Ringboxcopy.svg'
+import dron from '@/assets/dron.svg'
+import fly from '@/assets/fly.svg'
+// 2 section 
+import bussionconnection from '@/assets/bussionconnection.svg'
+import tree from '@/assets/tree.svg'
+import dotdotbox from '@/assets/dotdotbox.svg'
+import plant from '@/assets/plant.svg'
+import Prosperity from '@/assets/Prosperity.svg'
+import purpose from '@/assets/purpose.svg'
+import dotdotbox_1 from '@/assets/dotdotbox_1.svg'
+// 3 section
+import Imaginethepossibilitiessilder_1 from '@/assets/Imaginethepossibilitiessilder_1.png'
+import Imaginethepossibilitiessilder_2 from '@/assets/Imaginethepossibilitiessilder_2.png'
+import Imaginethepossibilitiessilder_3 from '@/assets/Imaginethepossibilitiessilder_3.png'
+import Imaginethepossibilitiessilder_4 from '@/assets/Imaginethepossibilitiessilder_4.png'
+import Imaginethepossibilitiessilder_5 from '@/assets/Imaginethepossibilitiessilder_5.png'
+import Imaginethepossibilitiessilder_6 from '@/assets/Imaginethepossibilitiessilder_6.png'
+// tabs
+import myEarthPoint from '@/assets/myEarthPoint.png'
+import myEarthPointlogo from '@/assets/myEarthPointlogo.svg'
+import JunktoJungles from '@/assets/JunktoJungles.png'
+import Junktoungleslogo from '@/assets/Junktoungleslogo.svg'
+import SeedsofRecognition from '@/assets/SeedsofRecognition.png'
+import SeedsofRecognitionlogo from '@/assets/SeedsofRecognitionlogo.svg'
+import BrandaHabitat from '@/assets/BrandaHabitat.png'
+import BrandaHabitatlogo from '@/assets/BrandaHabitatlogo.svg'
+
+// 4 section
+import growyourbussion from '@/assets/growyourbussion.svg'
+import Businesseswereimaginlogo from '@/assets/Businesseswereimaginlogo.svg'
+import Businessesreimagined_1 from '@/assets/Businessesreimagined_1.svg'
+import Businessesreimagined_2 from '@/assets/Businessesreimagined_2.svg'
+import Businessesreimagined_3 from '@/assets/Businessesreimagined_3.svg'
+import Businessesreimagined_4 from '@/assets/Businessesreimagined_4.svg'
+import Businessesreimagined_5 from '@/assets/Businessesreimagined_5.svg'
+import Businessesreimagined_6 from '@/assets/Businessesreimagined_6.svg'
+import Businessesreimagined_7 from '@/assets/Businessesreimagined_7.svg'
+import Businessesreimagined_8 from '@/assets/Businessesreimagined_8.svg'
+import Businessesreimagined_9 from '@/assets/Businessesreimagined_9.svg'
+import Businessesreimagined_10 from '@/assets/Businessesreimagined_10.svg'
+import Businessesreimagined_11 from '@/assets/Businessesreimagined_11.svg'
 
 export const BusinessForPlanet: React.FC = () => {
   return (
--- src/pages/PeopleForPlanet/PeopleForPlanet.tsx
+++ src/pages/PeopleForPlanet/PeopleForPlanet.tsx
@@ -3,6 +3,52 @@
  */
 import { SEO } from '@/components/common/SEO'
 import styles from './PeopleForPlanet.module.css'
+// 1 section
+import PeopleforthePlanetlogo from '@/assets/PeopleforthePlanetlogo.png'
+// 2 section
+import BecomeaGuardian_1 from '@/assets/BecomeaGuardian_1.png'
+import BecomeaGuardian_2 from '@/assets/BecomeaGuardian_2.png'
+import BecomeaGuardianlogo from '@/assets/BecomeaGuardianlogo.svg'
+import sun from '@/assets/sun.svg'
+import butterfly from '@/assets/butterfly.svg'
+import frog from '@/assets/frog.svg'
+import connection from '@/assets/connection.svg'
+import arrow from '@/assets/arrow.svg'
+// 3 section
+import Wesecurebiodi from '@/assets/Wesecurebiodi.svg'
+import SacredGroglobalscale from '@/assets/SacredGroglobalscale.png'
+import onesecurelogo from '@/assets/onesecurelogo.svg'
+import twosecurelogo from '@/assets/twosecurelogo.svg'
+import threesecurelogo from '@/assets/threesecurelogo.svg'
+import foursecurelogo from '@/assets/foursecurelogo.svg'
+import foursecurelogo_1 from '@/assets/foursecurelogo_1.svg'
+import fivesecurelogo from '@/assets/fivesecurelogo.svg'
+import playbutton from '@/assets/playbutton.svg'
+// 4 section 
+import ProtectaHabitat from '@/assets/ProtectaHabitat.svg'
+import ImpactCompass from '@/assets/ImpactCompass.png'
+import BritishPoundbackground from '@/assets/BritishPoundbackground.svg'
+import BritishPoundlogo from '@/assets/BritishPoundlogo.svg'
+
+// 5 section 
+import GiftaHabitatilogo from '@/assets/GiftaHabitatilogo.svg'
+import GiftaHabitatimage from '@/assets/GiftaHabitatimage.png'
+import GiftaHabitat_1 from '@/assets/GiftaHabitat_1.svg'
+import GiftaHabitat_2 from '@/assets/GiftaHabitat_2.svg'
+import GiftaHabitat_3 from '@/assets/GiftaHabitat_3.svg'
+import GiftaHabitat_4 from '@/assets/GiftaHabitat_4.svg'
+import GiftaHabitat_5 from '@/assets/GiftaHabitat_5.svg'
+import GiftaHabitat_6 from '@/assets/GiftaHabitat_6.svg'
+import GiftaHabitat_7 from '@/assets/GiftaHabitat_7.svg'
+
+// 6 section
+import Sacreddeedsmust from '@/assets/Sacreddeedsmust.svg'
+import Certificateimage from '@/assets/Certificateimage.svg'
+import certificateimagelogo from '@/assets/certificateimagelogo.svg'
+import VirtualBackground from '@/assets/VirtualBackground.svg'
+import virtualbacklogo from '@/assets/virtualbacklogo.png'
+import virtualbackcenterlogo_1 from '@/assets/virtualbackcenterlogo_1.svg'
+import virtualbackcenterlogo_2 from '@/assets/virtualbackcenterlogo_2.svg'
 
 export const PeopleForPlanet: React.FC = () => {
   return (
